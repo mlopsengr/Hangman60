9c52e7223e5373a10ce0c584fab912c6da9fbaa6
Tobi John <66161461+tobsiee@users.noreply.github.com>
2022-06-04 20:17:35 +0100

    commit 1

--- hangman.py
+++ hangman.py
@@ -131,6 +131,6 @@ def play_game(word_list):
     pass
 
 if __name__ == '__main__':
-    word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
+    word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon'] #words posible
     play_game(word_list)
 # %%
