7335c94c9b45781dca6bac3738406584ad95e739
Tobi John <66161461+tobsiee@users.noreply.github.com>
2022-06-05 19:52:06 +0100

    removes unnecasary comment

--- hangman.py
+++ hangman.py
@@ -131,6 +131,6 @@ def play_game(word_list):
     pass
 
 if __name__ == '__main__':
-    word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon'] # words to be guessed
+    word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon'] 
     play_game(word_list)
 # %%
