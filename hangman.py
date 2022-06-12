15497711cf671fed791e167f89d11305956d4580
Tobi John <66161461+tobsiee@users.noreply.github.com>
2022-06-12 19:04:22 +0100

    new edits

--- hangman.py
+++ hangman.py
@@ -135,4 +135,4 @@ if __name__ == '__main__':
     play_game(word_list)
 # %%
 
-# ensure all works, need to send link to aicore instructor for review
+# ensure all works, need to send link to aicore instructor
