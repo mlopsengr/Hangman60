b0389f3eccd13d40e19220c5edbe90244498b160
Tobi John <66161461+tobsiee@users.noreply.github.com>
2022-06-23 21:38:55 +0100

    extra edits

--- hangman.py
+++ hangman.py
@@ -135,7 +135,7 @@ if __name__ == '__main__':
     play_game(word_list)
 # %%
 
-# ensure all works for scoring and tick tasks con portal, need to send link to aicore instructor for review (Bola) by monday
+# ensure all works for scoring and tick tasks con portal, need to send link to aicore instructor for review (Bola) by monday - done
 
 # retry github desktop for pushing
 ##
